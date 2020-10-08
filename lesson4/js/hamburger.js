95f1dce86c969d7c431ee4f407226999a3690935
goodmik15 <goodrich.kim@hotmail.com>
2020-10-07 22:13:45 -0600

    lesson4 css update

--- lesson4/js/hamburger.js
+++ lesson4/js/hamburger.js
@@ -1,4 +1,4 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('navigation')
+const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
